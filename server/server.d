95b0eead75d4fcb8ec4527fbc710101f5c6c76c8
lmartin92 <lmartin92@997a04aa-7115-428f-bf60-26c17c37e761>
2008-10-26 12:35:55 +0000

    Fixed up the reciever......... now it's a monitor of each port that is being served by a thread

--- server/server.d
+++ server/server.d
@@ -6,15 +6,24 @@ module server.server;
 private {
 	import tango.core.Thread;
 	import tango.io.device.ThreadConduit;
+	import tango.net.ServerSocket;
 }
 
 alias void delegate(int, int) UnSubscribe;
+alias void[] delegate(void[], bool) Encoder; //the boolean tells whether to encode or decode
 
 // all Subscribers must implement this class
 abstract class Subscriber: Thread {
-	ThreadConduit notifiable, information, notify;
+	public ThreadConduit notifiable, information, notify, push; 
+	// notifiable; what the server writes to for is alive requests
+	// information; what Reciever writes to for giving information
+	// notify; what this writes to to let server know is alive
+	// push; what this writes to to put data on the internet
 	UnSubscribe unSubscribe;
+	
+public:
 	uint port;
+	int upper, lower;
 	
 	this() {
 		auto conduits = [new ThreadConduit, new ThreadConduit];
@@ -24,31 +33,118 @@ abstract class Subscriber: Thread {
 	this(ThreadConduit information_) {
 		notifiable = new ThreadConduit;
 		information = information_;
+		
+		super(&notifyLoop);
 	}
 	
 	void unRegister(UnSubscribe unSubscribe) {
 		this.unSubscribe = unSubscribe;
 	}
+
+	//all subclasses must implmement their threaded logic here
+	abstract void run();
 	
-	void notify(ThreadConduit notify) {
-		this.notify = notify;
+	void notifyLoop() {
+		char[] to_read;
+		for(; ;) {
+			notifiable.read(to_read);
+			if(read = "alive") {
+				notify.write("alive");
+			}
+			notifiable.clear();
+			
+			run();
+		}
 	}
+	
+	
 }
 
-class Reciever: Thread {
+private class Handler : Thread { // this will connect and listen on certain ports
+						 // must modify it to actually do this
+	public ThreadConduit notifiable, notify;
+	public Subscriber[int][int] subscribers;
+	public uint port;
+	
+	public this(uint port) {
+		this.port = port;
+		notifiable = new ThreadConduit;
+	}
+	
+	public void add(Subscriber subscriber) {
+		subscribers[subscriber.upper][subscriber.lower] = subscriber;
+	}	
+}
+
+private class Reciever: Thread {
 	Subscriber[int][int] subscribers;
-	uint[] ports;
+	Handler[uint] handlers;
+	Encoder encode;
+	ThreadConduit notify;
 	
 public:
-	this(Subscriber[int][int] subscribers) {
+	this(Subscriber[int][int] subscribers, Encoder encode) {
 		this.subscribers = subscribers;
-		foreach(subscriber; subscribers)
-			ports ~= subscriber.port;
+		this.encode = encode;
+		
+		for(int i = 0; i < subscribers.length; i++) {
+			for(int j = 0; j < subscribers[i].length; i++) {
+				subscribers[i][j].upper = i;
+				subscribers[i][j].lower = j;
+			}
+		}
+		
+		foreach(subscriber; subscribers) {
+			if(handlers[subscriber.port] !is null)
+				handlers[subscriber.port].add(subscriber);
+			else {
+				handlers[subscriber.port] = new Handler(subscriber.port);
+				handlers[subscriber.port].add(subscriber);
+			}
+		}
+		
+		foreach(handle; handlers)
+			handle.notify(notify);
+						
+		super(&run);
+	}
+	
+private:
+	void run() {
+		foreach(handler; handlers)
+			handler.start;
+		
+		this.sleep(5);
+		char[] to_read;
+		Subscriber[int][int] temp;
+		uint portTemp;
+		for(;;) { //monitor the handlers here and restart them if necessary
+			foreach(handle; handlers) {
+				notify.clear;
+				handle.notifiable.write("alive");
+				this.sleep(10);
+				notify.read(to_read);
+				if(to_read != "alive") {
+					temp = handle.subscribers;
+					portTemp = handle.port;
+					
+					delete handle;
+					handle = null;
+					
+					handle = new Handler(portTemp);
+					handle.subscribers = temp;
+					
+					handle.start;
+					this.sleep(5);
+				}
+			}
+		}
 	}
 
 }
 
-class Sender: Thread {
+private class Sender: Thread { // must modify this to actually send stuff
+							   // must notify all stuff of where to put data to
 	Subscriber[int][int] subscribers;
 	
 }
@@ -60,15 +156,17 @@ class Server: Thread {
 	Subscriber[int][int] subscribers;
 	ThreadConduit[] conduits;
 	ThreadConduit notify;
+	Encoder encode;
 
 public:
-	this() {
-		this(null);
+	this(Encoder encode) {
+		this(null, encode);
 	}
 	
-	this(Subscriber[int][int] subscribers_) {
-		reciever = new Reciever(subscribers);
-		sender = new Sender(subscribers);
+	this(Subscriber[int][int] subscribers_, Encoder encode) {
+		this.encode = encode;
+		reciever = new Reciever(subscribers, encode);
+		sender = new Sender(subscribers, encode);
 		
 		if(subscribers_ !is null)
 			subscribers = subscribers_;
@@ -84,13 +182,17 @@ public:
 	void register(int upper, int lower, Subscriber subscribers) {
 		subscribers[upper][lower] = subscriber;
 	}
-	
+
 private:
 	void unRegister(int upper, int lower) {
 		subscribers[upper][lower] = null;
 	}
 	
 	void run() {
+		sender.start;
+		reciever.start;
+		this.sleep(5);
+		
 		for(;;) {
 			foreach(subscriber; subscribers) {
 				if(subscriber !is null) {
