2884ce3238c1ad2a4b06d7b2a1d185d4adf8b78d
lmartin92 <lmartin92@997a04aa-7115-428f-bf60-26c17c37e761>
2009-07-08 04:14:51 +0000

    Got rid of the old non-optimal one, now a more optimal one is in place

--- trunk/src/threading/ThreadManager.d
+++ trunk/src/threading/ThreadManager.d
@@ -13,6 +13,7 @@ private {
 
 alias void delegate() TaskDG;
 
+/+ old
 private class TaskList {
 private:
     class Node {
@@ -261,27 +262,360 @@ public:
     synchronized void steal(int task) {
             pushTo = task;
     }
+} +/
+
+private class TaskList {
+private:
+    Node        head,
+                current;
+    uint        count = 0;
+
+    class Node {
+    public:
+        TaskDG  dg;
+        Node    next;
+
+        this(TaskDG task) {
+            dg = task;
+        }
+    }
+
+public:
+    bool empty() {
+        return count == 0;
+    }
+
+    void push(TaskDG task) {
+        count++;
+
+        if(head is null) {
+            head = new Node(task);
+            current = head;
+        } else {
+            current.next = new Node(task);
+            current = current.next;
+        }
+    }
+
+    TaskDG pop() {
+        count = count != 0 ? count - 1 : count;
+
+        TaskDG temp = null;
+
+        if(head !is null) {
+            temp = head.dg;
+            if(head.next !is null)
+                head = head.next;
+            else
+                head = null;
+        }
+
+        return temp;
+    }
+
+    uint size() {
+        return count;
+    }
+}
+
+private class TaskStore {
+private:
+    TaskList[int]       lists;
+    Mutex[int]          listsMutex;
+    uint[int]           count;
+    int                 numList;
+
+public:
+    this(int listNum) {
+        numList = listNum;
+
+        for(int i = 0; i < numList; i++) {
+            lists[i] = new TaskList;
+            listsMutex[i] = new Mutex(lists[i]);
+            count[i] = 0;
+        }
+    }
+
+    void push(TaskDG task) {
+        bool did            = false;
+        bool lockManaged    = false;
+        int  used;
+    
+        if(task !is null) {
+            for(int i = 0; i < numList; i++) {
+                lockManaged = listsMutex[i].tryLock;
+
+                if(lockManaged == true) {
+                    lists[i].push(task);
+                    count[i]++;
+                    listsMutex[i].unlock;
+                    did = true;
+                    break;
+                }
+            }
+
+            if(!did) {
+                used = count[0];
+
+                for(int i = 0; i < numList; i++) {
+                    if(used > count[i]) used = i;
+                }
+
+                listsMutex[used].lock;
+                    lists[used].push(task);
+                    count[used]++;
+                listsMutex[used].unlock;
+            }
+        }
+    }
+
+    TaskDG pop() {
+        TaskDG temp         = null;
+        bool   did          = false,
+               lockManaged  = false;
+        int    used;
+
+        for(int i = 0; i < numList; i++) {
+            lockManaged = listsMutex[i].tryLock;
+
+            if(lockManaged == true) {
+                temp = lists[i].pop();
+
+                if(temp is null) {
+                    listsMutex[i].unlock;
+                    continue;
+                } else {
+                    count[i]--;
+                    listsMutex[i].unlock;
+                    did = true;
+                    break;
+                }
+            }
+        }
+
+        if(!did) {
+            used = 0;
+            for(int i = 0; i < numList; i++) {
+                if(count[i] > used) used = i;
+            }
+
+            listsMutex[used].lock;
+                temp = lists[used].pop;
+                count[used]--;
+            listsMutex[used].unlock;
+        }
+
+        return temp;
+    }
+
+    bool empty() {
+        bool temp = true;
+
+        for(int i = 0; i < numList; i++) {
+            if(temp is true && count[i] == 0)
+                temp = true;
+            else temp = false;
+        }
+
+        return temp;
+    }
+
+    uint size() {
+        uint temp;
+
+        for(int i = 0; i < numList; i++) {
+            temp += count[i];
+        }
+
+        return temp;
+    }
+}
+
+private class Task: Thread {
+private:
+    Task            next;
+    int             threadNum;
+    ThreadManager   man;
+    int             _kill;
+    Mutex           killAccess;
+
+    void run() {
+        TaskDG temp;
+        int temp1;
+
+        void doIt() {
+            if(store.empty) {
+                if(next !is null) {
+                    temp = next.steal;
+
+                    if(temp !is null)
+                        temp();
+                }
+            } else {
+                temp = store.pop;
+                if(temp !is null)
+                    temp();
+            }
+        }
+
+        killAccess.lock;
+            temp1 = _kill;
+        killAccess.unlock;
+
+        while(temp1 != 1) {
+
+            if(temp1 != 0)
+                doIt();
+
+            if(temp1 == 2) {
+                while(!store.empty) {
+                    temp = store.pop;
+                    if(temp !is null)
+                        temp();
+                    else
+                        break;
+                }
+                break;
+            }
+
+            killAccess.lock;
+                temp1 = _kill;
+            killAccess.unlock;
+        }
+    }
+
+public:
+    TaskStore       store;
+
+    this(ThreadManager _man, int _threadNum) {
+        man = _man;
+        threadNum = _threadNum;
+        store = new TaskStore(threadNum + 1);
+        killAccess = new Mutex;
+        _kill = -1;
+
+        super(&run);
+    }
+
+    void Kill() {
+        killAccess.lock;
+            _kill = 1;
+        killAccess.unlock;
+    }
+
+    void pause() {
+        killAccess.lock;
+            _kill = 0;
+        killAccess.unlock;
+    }
+
+    void proceed() {
+        killAccess.lock;
+            _kill = -1;
+        killAccess.unlock;
+    }
+
+    void finish() {
+        killAccess.lock;
+            _kill = 2;
+        killAccess.unlock;
+    }
+
+    TaskDG steal() {
+        if(store.empty) {
+            if(next !is null)
+                return next.steal;
+        } else {
+            return store.pop;
+        }
+        return null;
+    }
+}
+
+public class ThreadManager {
+private:
+    Task[int]       tasks;
+    int             threadNum;
+
+public:
+
+    this(int numThread) {
+        threadNum = numThread;
+        Task prev;
+        for(int i = 0; i < threadNum; i++) {
+            tasks[i] = new Task(this, threadNum);
+            tasks[i].pause;
+            if(i != 0) {
+                prev.next = tasks[i];
+                prev = tasks[i];
+            } else
+                prev = tasks[i];
+        }
+    }
+
+    ~this() {
+        this.finish();
+        this.join();
+    }
+
+    void start() {
+        for(int i = 0; i < threadNum; i++) {
+            tasks[i].proceed;
+            tasks[i].start;
+        }
+    }
+
+    void kill() {
+        for(int i = 0; i < threadNum; i++) {
+            tasks[i].Kill;
+        }
+    }
+
+    void finish() {
+        for(int i = 0; i < threadNum; i++) {
+            tasks[i].finish;
+        }
+    }
+
+    void add(TaskDG task) {
+        int used = 0;
+
+        for(int i = 0; i < threadNum; i++) {
+            if(tasks[i].store.size < tasks[used].store.size)
+                used = i;
+        }
+
+        tasks[used].store.push(task);
+    }
+
+    void join() {
+        for(int i = 0; i < threadNum; i++) {
+            if(tasks[i] !is null)
+                tasks[i].join;
+        }
+    }
 }
 
 debug(ThreadManager) {
-            class temp {
-                int i3;
-                public:
-                void dont() {
-                    Trace.formatln("I'm a thread {}", i3);
-                }
-                this(int i2 = 0) {
-                    i3 = i2;
-                }
-            }
+    class Temp {
+        int i;
+
+    public:
+        this(int _i) { i = _i; }
+        void dont() {
+            Trace.formatln("I'm a thread {}", i);
+        }
+    }
+
     void main() {
-        scope man = new ThreadManager(4, 4);
-        man.start;
-        for(int i = 0; i < 10000000; i++) {
-            man.add(&(new temp(i)).dont);
+        auto man = new ThreadManager(4);
+
+        man.start();
+
+        for(int i = 0; i < 500000; i++) {
+            man.add(& ( new Temp(i) ).dont);
         }
 
-        Thread.sleep(1000);
-
+        delete man;
     }
-}
+} 
